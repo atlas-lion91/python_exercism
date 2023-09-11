1d12484eda7312b17db8b50d6ff5fd8537659c87
Khalil E <140761974+atlas-lion91@users.noreply.github.com>
2023-09-11 16:46:33 -0400

    Update leap.py

--- exercises/practice/leap/leap.py
+++ exercises/practice/leap/leap.py
@@ -1,2 +1,16 @@
 def leap_year(year):
-    pass
+    def leap_year(year):
+    """Check if a year is a leap year.
+
+    :param year: int - the year to check.
+    :return: bool - True if the year is a leap year, False otherwise.
+
+    This function checks if a year is a leap year based on the following rules:
+    1. It is evenly divisible by 4.
+    2. It is not evenly divisible by 100, except when it is also evenly divisible by 400.
+    """
+
+    if (year % 4 == 0 and year % 100 !=0) or (year % 400 == 0):
+        return True
+    else:
+        return False
